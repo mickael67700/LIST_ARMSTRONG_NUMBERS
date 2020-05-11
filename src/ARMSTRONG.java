e64ea75774fa0e0b5215eb158a11d45855e35c96
Mike <mickael.merlet67@gmail.com>
2020-05-11 13:41:54 +0200

    Constructeur classe et main

--- src/ARMSTRONG.java
+++ src/ARMSTRONG.java
@@ -5,45 +5,51 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ARMSTRONG {
-    public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            String str = null;
-            char ch;
-            // Nombre limite pour calculer les nombres d'Armstrong
-            System.out.println("Enter Limit : ");
-            int limit = sc.nextInt();
-            int temp = 0;
-            // Boucle de nombres de 1 à nombre limite
-            for (int number = 1; number <= limit; number++) {
-                // Somme des digits composant le nombre
-                int sum = 0;
-                // str = on récupère en String le int number
-                str = String.valueOf(number);
-                // Initialisation du tableau dans lequel sera stocké les puissances de chaque digit calculées
-                int[] digits = new int[str.length()];
-                // On parcours la chaine String number et à chaque caractère on récupère la valeur numérique.
-                for (int i = 0; i < str.length(); i++) {
-                    ch = str.charAt(i);
-                    //récupération de la valeur numérique correspondant au Char i de la chaine str
-                    int digit = Character.getNumericValue(ch);
-                    temp = digit;
-                    // On calcul les puissances.
-                    for (int puissance = 1; puissance < str.length(); puissance++) {
-                        digit = temp * digit;
-                        //Stockage des puissances en tableau
-                        digits[i] = digit;
-                    }
+    public ARMSTRONG(int limit) {
+        String str = null;
+        char ch;
+        // Nombre limite pour calculer les nombres d'Armstrong int limit
+
+        int temp = 0;
+        // Boucle de nombres de 1 à nombre limite
+        for (int number = 1; number <= limit; number++) {
+            // Somme des digits composant le nombre
+            int sum = 0;
+            // str = on récupère en String le int number
+            str = String.valueOf(number);
+            // Initialisation du tableau dans lequel sera stocké les puissances de chaque digit calculées
+            int[] digits = new int[str.length()];
+            // On parcours la chaine String number et à chaque caractère on récupère la valeur numérique.
+            for (int i = 0; i < str.length(); i++) {
+                ch = str.charAt(i);
+                //récupération de la valeur numérique correspondant au Char i de la chaine str
+                int digit = Character.getNumericValue(ch);
+                temp = digit;
+                // On calcul les puissances.
+                for (int puissance = 1; puissance < str.length(); puissance++) {
+                    digit = temp * digit;
+                    //Stockage des puissances en tableau
+                    digits[i] = digit;
                 }
-                // Calcul de la somme des puissances stockées
-                for (int digit : digits) {
-                    sum += digit;
-                }
-                // Si somme = nombre , alors c'est un Nombre d'Armstrong.
-                if (number == sum)
-                    System.out.println("Armstrong number : " + number);
             }
+            // Calcul de la somme des puissances stockées
+            for (int digit : digits) {
+                sum += digit;
+            }
+            // Si somme = nombre , alors c'est un Nombre d'Armstrong.
+            if (number == sum)
+                System.out.println("Armstrong number : " + number);
         }
     }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Limit : ");
+        int limit = sc.nextInt();
+        ARMSTRONG armstrong = new ARMSTRONG(limit);
+    }
+}
 
 
 
